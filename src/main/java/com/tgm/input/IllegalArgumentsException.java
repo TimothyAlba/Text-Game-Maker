1f22cbe11f03c43b926135e0c9af2829f0f607d2
Timothy Lloyd <timothyalba.official@gmail.com>
2019-07-09 17:11:16 -0500

    Deleted old code to start fresh for team code.

--- src/main/java/com/tgm/input/IllegalArgumentsException.java
+++
@@ -1,8 +0,0 @@
-package com.tgm.input;
-
-public class IllegalArgumentsException extends Exception {
-
-    public IllegalArgumentsException(String e){
-        super(e);
-    }
-}
--- src/test/java/Constants.java
+++
@@ -1,4 +0,0 @@
-public class Constants {
-
-    public static final String START_ID = "start";
-}
